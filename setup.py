fc39e617a8e52fee3c6d6e040eff0853c700277f
lshh125 <31879010+lshh125@users.noreply.github.com>
2020-09-27 23:21:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="",
     packages=setuptools.find_packages(),
+    install_requires=[
+        l.strip() for l in Path('requirements.txt').read_text('utf-8').splitlines()
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
